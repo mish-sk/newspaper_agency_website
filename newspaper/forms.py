7d8c9fdc279f8908c2b6bd9598db181b46a57fd5
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-04-10 17:58:15 +0200

    add RedactorForm

--- newspaper/forms.py
+++ newspaper/forms.py
@@ -1,3 +1,14 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from newspaper.models import Redactor
 
 
+class RedactorForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Redactor
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "years_of_experience"
+        )
