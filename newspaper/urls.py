88929da5ce989d0d06435726aa7f31ee0ece8500
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-04-10 16:56:16 +0200

    add CRUD for Topic model

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
 
-from newspaper.views import index
+from newspaper.views import (
+    index,
+    TopicListView,
+    TopicCreateView,
+    TopicUpdateView,
+    TopicDeleteView,
+
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path("topics/", TopicListView.as_view(), name="topic_list"),
+    path("topics/create/", TopicCreateView.as_view(), name="topic_create"),
+    path("topics/<int:pk>/update/", TopicUpdateView.as_view(), name="topic_update"),
+    path("topics/<int:pk>/delete/", TopicDeleteView.as_view(), name="topic_delete"),
 ]
 
 app_name = "newspaper"
--- newspaper/views.py
+++ newspaper/views.py
@@ -1,12 +1,43 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+
+from newspaper.models import Topic, Newspaper, Redactor
+from newspaper_agency_website import settings
 
 
 def index(request):
+    topics = Topic.objects.all()
+    newspapers = Newspaper.objects.all()
+    redactors = Redactor.objects.all()
     context = {
-
+        "topics": topics,
+        "newspapers": newspapers,
+        "redactors": redactors
     }
     return render(
         request,
         "newspaper/index.html",
         context=context
     )
+
+
+class TopicListView(generic.ListView):
+    model = Topic
+
+
+class TopicCreateView(generic.CreateView):
+    model = Topic
+    fields = "__all__"
+    success_url = reverse_lazy("newspaper:topic_list")
+
+
+class TopicUpdateView(generic.UpdateView):
+    model = Topic
+    fields = "__all__"
+    success_url = reverse_lazy("newspaper:topic_list")
+
+
+class TopicDeleteView(generic.DeleteView):
+    model = Topic
+    success_url = reverse_lazy("newspaper:topic_list")
