f1af19e4d30e0692bc935951bdf922454853b1d0
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-04-10 09:24:14 +0200

    add empty index to views.py

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {
+
+    }
+    return render(
+        request,
+        "newspaper/index.html",
+        context=context
+    )
