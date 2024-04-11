87cd26382e27c5f6d4777f7cd317f3918c24e89c
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-04-11 12:40:32 +0200

    add login required restriction to views.py

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
@@ -6,6 +8,7 @@ from newspaper.forms import RedactorForm
 from newspaper.models import Topic, Newspaper, Redactor
 
 
+@login_required
 def index(request):
     topics = Topic.objects.all()
     newspapers = Newspaper.objects.all()
@@ -22,71 +25,71 @@ def index(request):
     )
 
 
-class TopicListView(generic.ListView):
+class TopicListView(LoginRequiredMixin, generic.ListView):
     model = Topic
 
 
-class TopicCreateView(generic.CreateView):
-    model = Topic
-    fields = "__all__"
-    success_url = reverse_lazy("newspaper:topic_list")
-
-
-class TopicUpdateView(generic.UpdateView):
+class TopicCreateView(LoginRequiredMixin, generic.CreateView):
     model = Topic
     fields = "__all__"
     success_url = reverse_lazy("newspaper:topic_list")
 
 
-class TopicDeleteView(generic.DeleteView):
+class TopicUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Topic
+    fields = "__all__"
+    success_url = reverse_lazy("newspaper:topic_list")
+
+
+class TopicDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Topic
     success_url = reverse_lazy("newspaper:topic_list")
 
 
-class NewspaperListView(generic.ListView):
+class NewspaperListView(LoginRequiredMixin, generic.ListView):
     model = Newspaper
 
 
-class NewspaperCreateView(generic.CreateView):
-    model = Newspaper
-    fields = "__all__"
-    success_url = reverse_lazy("newspaper:newspaper_list")
-
-
-class NewspaperDetailView(generic.DetailView):
-    model = Newspaper
-
-
-class NewspaperUpdateView(generic.UpdateView):
+class NewspaperCreateView(LoginRequiredMixin, generic.CreateView):
     model = Newspaper
     fields = "__all__"
     success_url = reverse_lazy("newspaper:newspaper_list")
 
 
-class NewspaperDeleteView(generic.DeleteView):
+class NewspaperDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Newspaper
+
+
+class NewspaperUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Newspaper
+    fields = "__all__"
+    success_url = reverse_lazy("newspaper:newspaper_list")
+
+
+class NewspaperDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Newspaper
     success_url = reverse_lazy("newspaper:newspaper_list")
 
 
-class RedactorListView(generic.ListView):
+class RedactorListView(LoginRequiredMixin, generic.ListView):
     model = Redactor
 
 
-class RedactorCreateView(generic.CreateView):
+class RedactorCreateView(LoginRequiredMixin, generic.CreateView):
     model = Redactor
     form_class = RedactorForm
 
 
-class RedactorDetailView(generic.DetailView):
+class RedactorDetailView(LoginRequiredMixin, generic.DetailView):
     model = Redactor
 
 
-class RedactorUpdateView(generic.UpdateView):
+class RedactorUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Redactor
     form_class = RedactorForm
     success_url = reverse_lazy("newspaper:redactor_list")
 
 
-class RedactorDeleteView(generic.DeleteView):
+class RedactorDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Redactor
     success_url = reverse_lazy("newspaper:redactor_list")
