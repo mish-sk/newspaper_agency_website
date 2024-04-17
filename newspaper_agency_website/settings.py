4754993a2d858cfd4644d6e1f8d7b6f07cb9e642
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-04-17 18:38:56 +0200

    add Crispy Forms Bootstrap 5

--- newspaper_agency_website/settings.py
+++ newspaper_agency_website/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "newspaper",
 ]
 
@@ -132,3 +134,7 @@ AUTH_USER_MODEL = "newspaper.Redactor"
 LOGIN_REDIRECT_URL = "/"
 
 LOGOUT_REDIRECT_URL = "/"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
