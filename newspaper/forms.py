536c08d4c00ff4d9082b0c2f0099a88faaaa5142
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-04-18 15:53:47 +0200

    add search forms to forms.py

--- newspaper/forms.py
+++ newspaper/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.forms.widgets import CheckboxSelectMultiple
 
-from newspaper.models import Redactor, Newspaper
+from newspaper.models import Redactor, Newspaper, Topic
 
 
 class RedactorForm(UserCreationForm):
@@ -23,3 +23,36 @@ class NewspaperForm(forms.ModelForm):
             'publishers': CheckboxSelectMultiple,
             'published_date': forms.DateInput(attrs={'type': 'date'}),
         }
+
+
+class TopicSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "search topic"})
+    )
+
+
+class RedactorSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "search first_name"})
+    )
+
+
+class NewspaperSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by Title or Content"})
+    )
+    topic = forms.ModelChoiceField(
+        queryset=Topic.objects.all(),
+        required=False,
+        label="Filter by Topic",
+        empty_label="All Topics"
+    )
