a22786aaf3c86fba0522017d656e3e0687fcb7a0
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-04-22 15:56:36 +0200

    update requirements.txt

--- newspaper_agency_website/settings.py
+++ newspaper_agency_website/settings.py
@@ -26,11 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'RENDER' not in os.environ
+DEBUG = "RENDER" not in os.environ
 
 
 ALLOWED_HOSTS = [
