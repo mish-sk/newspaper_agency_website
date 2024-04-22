bb4e1a4290923caa0e817bf6769c4f7983338553
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-04-22 14:47:54 +0200

    update settings.py

--- newspaper_agency_website/settings.py
+++ newspaper_agency_website/settings.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os
+
 import dj_database_url
 
 from pathlib import Path
 from dotenv import load_dotenv
-from decouple import config
 
 load_dotenv()
 
@@ -25,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = []
 
